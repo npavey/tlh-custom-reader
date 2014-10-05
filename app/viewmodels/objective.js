5fe6e313c19e392c5cfb18ea3dae1701ea8aa696
Andrey Drebot <a.drebot@gmail.com>
2014-10-06 00:57:48 +0300

    #7: Back-next navigation between pages was added

--- app/viewmodels/objective.js
+++ app/viewmodels/objective.js
@@ -1,9 +1,13 @@
 ﻿define(['knockout', 'plugins/router', 'dataContext'], function (ko, router, dataContext) {
+    "use strict";
 
     var ViewModel = function () {
         this.id = ko.observable();
         this.isTableOfContentVisible = ko.observable(true);
         this.pages = ko.observableArray([]);
+        this.previousPage = ko.observable();
+        this.currentPage = ko.observable();
+        this.nextPage = ko.observable();
         this.router = router.createChildRouter()
                 .makeRelative({
                 }).map([
@@ -12,7 +16,7 @@
     }
 
     ViewModel.prototype.canActivate = function (objectiveId, page) {
-        if (typeof page != "string") {
+        if (typeof page !== "string") {
             return { 'redirect': 'objective/' + objectiveId + '/page/' + dataContext.getObjective(objectiveId).pages[0].id };
         }
 
@@ -20,22 +24,28 @@
     }
 
     ViewModel.prototype.activate = function (objectiveId) {
-        var objective = dataContext.getObjective(objectiveId);
-        this.id(objective.id);
-        this.pages(objective.pages.map(function (page) {
-            return {
-                id: page.id,
-                title: page.title,
-                isActive: ko.computed(function () {
-                    if (router.activeInstruction() && router.activeInstruction().fragment && router.activeInstruction().fragment.length) {
-                        var url = router.activeInstruction().fragment;
-                        return url.indexOf(page.id, url.length - page.id.length) !== -1;
-                    }
-                    return false;
-                })
-            }
-        }));
+        var
+            that = this,
+            url = router.activeInstruction().fragment,
+            objective = dataContext.getObjective(objectiveId)
+        ;
 
+        that.id(objective.id);
+        that.pages(objective.pages.map(function (page) { return { id: page.id, title: page.title } }));
+        that.pages().forEach(function (page, index, array) {
+            if (url.indexOf(page.id, url.length - page.id.length) === -1) {
+                return;
+            }
+            that.previousPage(index > 0 ? array[index - 1] : null);
+            that.currentPage(page);
+            that.nextPage(index < array.length ? array[index + 1] : null);
+        });
+    }
+
+    ViewModel.prototype.deactivate = function () {
+        this.previousPage(null);
+        this.currentPage(null);
+        this.nextPage(null);
     }
 
     ViewModel.prototype.showTableOfContent = function () {
