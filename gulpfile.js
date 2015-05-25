b5c29fb92d87bd9a122c9c6d3e515282e14a1962
LeonidPelyhivskyi <plo.91ua@gmail.com>
2015-05-25 09:59:32 +0300

    Incorrect hint arrow layout in IE and Android 4.2.2.

--- gulpfile.js
+++ gulpfile.js
@@ -80,7 +80,7 @@ gulp.task('build-app', ['clean'], function () {
       .pipe(uglify())
       .pipe(gulp.dest(output + '/js'));
     
-     gulp.src(['./js/lib/hotspotOnImagePlugin/hotspotOnImagePlugin.css'])
+     gulp.src(['./js/lib/hotspotOnImagePlugin/hotspotOnAnImagePlugin.css'])
         .pipe(addBuildVersion())
         .pipe(minifyCss({ keepBreaks: true }))
         .pipe(gulp.dest(output + '/js/lib/hotspotOnImagePlugin'));
