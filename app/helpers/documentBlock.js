66a470e5f06f30b0633b5d4bf27b0b681fef7f15
Oleksandr Bulakh <o.bulakh@easygenerator.com>
2017-08-03 13:23:39 +0300

    Fix document block

--- app/helpers/documentBlock.js
+++ app/helpers/documentBlock.js
@@ -9,51 +9,52 @@ define(['translation'], function (translation) {
         },
         downloadLocalizationKey: '[download]',
         containerSelector: '.document-container',
-        sizeAttrName: 'data-document-size-kb',
+        sizeAttrName: 'data-document-size',
         typeAttrName: 'data-document-type',
-        downloadBtnSelector: '.download-document-btn'
+        downloadBtnSelector: '.download-document-btn',
+        documentTitleWrapperSelector: '.document-title-wrapper'
     };
 
     return {
         getDocumentBlockContent: function(html) {
-            var $output = $('<output>').html(html);
+            var $output = $('<output>').html(html),
+                    $container = $output.find(constants.containerSelector);
+
             var downloadText = translation.getTextByKey(constants.downloadLocalizationKey);
-            var $container = $output.find(constants.containerSelector);
-            var documentType = $container.attr(constants.typeAttrName);
-            var documentSizeValue = $container.attr(constants.sizeAttrName);
-            var documentSize = getSize(documentSizeValue);
-            var downloadBtnText = downloadText + ' (' + documentSize + ')';
-            var $downloadBtn = $output.find(constants.downloadBtnSelector);
-            $downloadBtn.text(downloadBtnText);
-            var $typeIcon = $('<div class="icon-container"></div>');
-            $typeIcon.append('<span class="document-type-text">' + documentType + '</span>');
-            switch (documentType) {
-                case constants.types.zip: {
-                    $typeIcon.addClass('icon-zip');
-                    break;
-                }
-                default: {
-                    $typeIcon.addClass('icon-file');
-                    break;
-                }
-            }
-            var $typeIconWrapper = $('<div class="document-icon"></div>');
-            $typeIconWrapper.append($typeIcon);
-            $container.prepend($('<div class="separator"></div>'));
-            $container.prepend($typeIconWrapper);
+            var documentData = {
+                type: $container.attr(constants.typeAttrName),
+                size: +$container.attr(constants.sizeAttrName)
+            };
+
+            var documentSizeString = getSize(documentData.size);
+            var downloadBtnText = downloadText + ' (' + documentSizeString + ')';
+                
+             $output.find(constants.downloadBtnSelector)
+                .text(downloadBtnText);
+
+            var iconClass = documentData.type === constants.types.zip ? 'icon-zip' : 'icon-file';
+            var $typeIcon = $('<div class="icon-container">' +
+                '<span class="document-type-text">' + documentData.type + '</span>' +
+                '</div>')
+                .addClass(iconClass);
+            var $typeIconWrapper = $('<div class="document-icon"></div>')
+                .append($typeIcon);
+
+            var $documentInfo = $(constants.documentTitleWrapperSelector, $container)
+                .prepend($typeIconWrapper);
+
             var content = $output.children()[0];
             return content;
         }
     };
 
-    function getSize(sizeKb) {
-        var size = '';
-        if (!sizeKb) {
+    function getSize(size) {
+        var sizeStr = '';
+        if (!size || size < 1024) {
             return '0 Kb';
         }
-        if (sizeKb > 1024) {
-            size = (sizeKb / 1024).toFixed(2);
-        }
-        return size + ' MB';
+        sizeStr = (size / (1024 * 1024)).toFixed(2);
+
+        return sizeStr + ' MB';
     }
 });
